b21c90887a38a2a24b33cd564586e29fd8746743
scottdomes <scott@musefind.com>
2019-02-11 17:50:19 -0800

    Message list

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MessageList v-bind:messages="messages"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MessageList from './components/MessageList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    MessageList
+  },
+  data: () => ({
+    messages: []
+  })
 }
 </script>
 
